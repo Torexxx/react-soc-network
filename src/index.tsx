f81e6c0d8f4e1f094922fe1778c4d96d674f6d47
torexxx <molento-studio@yandex.ru>
2021-03-25 16:21:56 +0700

    fix indent

--- src/index.tsx
+++ src/index.tsx
@@ -6,7 +6,6 @@ import MainApp from './App';
 
 ReactDOM.render(<MainApp/>, document.getElementById('root'));
 
-
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
