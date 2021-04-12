982b9f292a0befb795b1cd5051854fd8a192b41a
torexxx <molento-studio@yandex.ru>
2021-04-12 19:47:54 +0700

    Header refactoring

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
 import React from 'react';
 import './App.css';
 import 'antd/dist/antd.css';
-import { Layout, Menu, Breadcrumb } from 'antd';
-import { UserOutlined, LaptopOutlined, NotificationOutlined } from '@ant-design/icons';
-import {BrowserRouter as Router, Redirect, Route, Switch, withRouter} from "react-router-dom";
-import HeaderContainer from './components/Header/HeaderContainer';
-import Navbar from './components/Navbar/Navbar';
+import {Breadcrumb, Layout, Menu} from 'antd';
+import {UserOutlined} from '@ant-design/icons';
+import {BrowserRouter as Router, Link, Redirect, Route, Switch, withRouter} from "react-router-dom";
+import {Header} from './components/Header/Header';
 import UsersPage from "./components/Users/UsersPage";
 // import MainAppHoc from "./hooks/mainAppHoc";
 // import MainAppChildren from "./hooks/mainAppChildren";
@@ -14,16 +13,16 @@ import {LoginPage} from './components/Login/LoginPage';
 import {Preloader} from "./components/common/Preloader/Preloader";
 import {connect, Provider} from 'react-redux';
 import {initializeApp} from "./redux/app-reducer";
-import { compose } from 'redux';
+import {compose} from 'redux';
 import store, {AppStateType} from './redux/redux-store';
 import withSuspense from "./hoc/withSuspense";
-import Button from 'antd/lib/button';
+
 const DialogsContainer = React.lazy(() => import("./components/Dialogs/DialogsContainer"));
 const ProfilePage = React.lazy(() => import("./components/Profile/ProfilePage"));
 
 
 const { SubMenu } = Menu;
-const { Header, Content, Footer, Sider } = Layout;
+const { Content, Footer, Sider } = Layout;
 
 type MapDispatchProps = { initializeApp: () => void };
 type MapStateProps = ReturnType<typeof mapStateToProps>;
@@ -53,14 +52,7 @@ class App extends React.Component<MapStateProps & MapDispatchProps> {
         if (this.props.initialized) {
             return (
                 <Layout>
-                    <Header className="header">
-                        <div className="logo" />
-                        <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['2']}>
-                            <Menu.Item key="1">nav 1</Menu.Item>
-                            <Menu.Item key="2">nav 2</Menu.Item>
-                            <Menu.Item key="3">nav 3</Menu.Item>
-                        </Menu>
-                    </Header>
+                    <Header />
                     <Content style={{ padding: '0 150px' }}>
                         <Breadcrumb style={{ margin: '16px 0' }}>
                             <Breadcrumb.Item>Home</Breadcrumb.Item>
@@ -71,27 +63,16 @@ class App extends React.Component<MapStateProps & MapDispatchProps> {
                             <Sider className="site-layout-background" width={200}>
                                 <Menu
                                     mode="inline"
-                                    defaultSelectedKeys={['1']}
+                                    defaultSelectedKeys={['2']}
                                     defaultOpenKeys={['sub1']}
                                     style={{ height: '100%' }}
                                 >
-                                    <SubMenu key="sub1" icon={<UserOutlined />} title="subnav 1">
-                                        <Menu.Item key="1">option1</Menu.Item>
-                                        <Menu.Item key="2">option2</Menu.Item>
-                                        <Menu.Item key="3">option3</Menu.Item>
-                                        <Menu.Item key="4">option4</Menu.Item>
+                                    <SubMenu key="sub1" icon={<UserOutlined />} title="My Profile">
+                                        <Menu.Item key="1"><Link to='/profile'>Profile</Link></Menu.Item>
+                                        <Menu.Item key="2"><Link to='/dialogs'>Messages</Link></Menu.Item>
                                     </SubMenu>
-                                    <SubMenu key="sub2" icon={<LaptopOutlined />} title="subnav 2">
-                                        <Menu.Item key="5">option5</Menu.Item>
-                                        <Menu.Item key="6">option6</Menu.Item>
-                                        <Menu.Item key="7">option7</Menu.Item>
-                                        <Menu.Item key="8">option8</Menu.Item>
-                                    </SubMenu>
-                                    <SubMenu key="sub3" icon={<NotificationOutlined />} title="subnav 3">
-                                        <Menu.Item key="9">option9</Menu.Item>
-                                        <Menu.Item key="10">option10</Menu.Item>
-                                        <Menu.Item key="11">option11</Menu.Item>
-                                        <Menu.Item key="12">option12</Menu.Item>
+                                    <SubMenu key="sub2" icon={<UserOutlined />} title="Developers">
+                                        <Menu.Item key="1"><Link to='/developers'>Developers</Link></Menu.Item>
                                     </SubMenu>
                                 </Menu>
                             </Sider>
@@ -100,7 +81,7 @@ class App extends React.Component<MapStateProps & MapDispatchProps> {
                                     <Route path='/' exact render={() => <Redirect to='/profile'/>}/>
                                     <Route path='/dialogs' render={() => <SuspendedDialogs /> }/>
                                     <Route path='/profile/:userId?' render={() => <SuspendedProfile />}/>
-                                    <Route path='/users' render={ () => <UsersPage titleText = {"Пользователи"}/> }/>
+                                    <Route path='/developers' render={ () => <UsersPage titleText = {"Developers"}/> }/>
                                     <Route path='/hooks' render={ () => <MainAppRenderProps /> }/>
                                     <Route path='/login' render={ () => <LoginPage /> }/>
                                     <Route path='*' render={ () => <div>404 NOT FOUND</div> }/>
@@ -108,7 +89,7 @@ class App extends React.Component<MapStateProps & MapDispatchProps> {
                             </Content>
                         </Layout>
                     </Content>
-                    <Footer style={{ textAlign: 'center' }}>Ant Design ©2018 Created by Ant UED</Footer>
+                    <Footer style={{ textAlign: 'center' }}>Social Network ©2021</Footer>
                 </Layout>
 
             )
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,32 +1,58 @@
 import React from 'react';
 import s from './Header.module.css';
-import {NavLink} from "react-router-dom";
-import logo from '../../assets/images/logo.png'
-import { UserAuthData } from './HeaderContainer';
+import {Link, NavLink} from "react-router-dom";
+import {UserOutlined} from '@ant-design/icons';
+import {Avatar, Col, Layout, Menu, Row} from "antd";
+import {useDispatch, useSelector} from "react-redux";
+import {AppStateType} from "../../redux/redux-store";
+import {logout} from "../../redux/auth-reducer";
 
-export type MapProps  = {
-    userAuthData: UserAuthData
-}
-export type DispatchProps  = {
-    logout: () => void
-}
-const Header: React.FC<MapProps & DispatchProps> = ({...props}) => {
+// export type UserAuthData = {
+//     userId:  number | null
+//     login:  string | null
+//     email:  string | null
+//     isAuth: boolean,
+//     userAvatar: string | null
+//     captchaUrl: string | null
+// }
 
-    const logOutHandler = () => {
-        props.logout();
+// export type MapProps = {
+//     userAuthData: UserAuthData
+// }
+// export type DispatchProps = {
+//     logout: () => void
+// }
+
+export const Header: React.FC = () => {
+
+    const userAuthData = useSelector((state : AppStateType) => state.auth);
+    const dispatch = useDispatch();
+
+    const logoutUser = () => {
+        dispatch(logout());
     }
 
-    return <header className={s.header}>
+    const {Header} = Layout;
 
-        <img alt="logo" className={s.logo} src={logo}/>
+    const logOutHandler = () => {
+        logoutUser();
+    }
 
-        <div className={s.loginBlock}>
-        { props.userAuthData.isAuth
-            ? <NavLink to={'/login'} onClick={logOutHandler} className={s.login} >Logout </NavLink>
-            : <NavLink to={'/login'} className={s.login}>Login</NavLink>
-        }
-        </div>
-    </header>
+    return <Header className="header">
+        <div className="logo"/>
+        <Row>
+            <Col span={20}>
+                <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['2']}>
+                    <Menu.Item key="1"><Link to='/developers'>Developers</Link></Menu.Item>
+                </Menu>
+            </Col>
+            <Col span={4}>
+                {userAuthData.isAuth
+                    ? <NavLink to={'/login'} onClick={logOutHandler} className={s.login}>Logout </NavLink>
+                    : <NavLink to={'/login'} className={s.login}>Login</NavLink>
+                }
+                <Avatar style={{backgroundColor: '#87d068'}} icon={<UserOutlined/>}/> {userAuthData.login}
+            </Col>
+        </Row>
+    </Header>
 }
-
-export default Header;
